37bafa07fe1ee2ce44c90fec6c027d16a49de6af
mjmolieleng@gmail.com <mjmolieleng@gmail.com>
2022-06-01 00:43:23 +0200

    added a timer using useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
+function useTimer() {
+  const [count, setCount] = useState(0);
+  const [stopped, setStopped] = useState();
+
+  function stopCounter() {
+    setStopped(true);
+    setCount(0);
+    console.log(`Sopped at ${count}`);
+  }
+
+  function startCounter() {
+    setStopped(false);
+  }
+
+  useEffect(() => {
+    let interval;
+    if (!stopped) {
+      interval = setInterval(() => {
+        setCount((prev) => prev + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [stopped]);
+
+  return { count, stopCounter, startCounter };
+}
 function App() {
+  const timer = useTimer();
+
+  const start = () => {
+    console.log("start");
+    timer.startCounter();
+  };
+
+  const stop = () => {
+    console.log("stopping");
+    timer.stopCounter();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>{timer.count}</h1>
+
+      <div>
+        <button
+          onClick={stop}
+          style={{
+            border: "0px",
+            background: "#333",
+            margin: "60px",
+            padding: "20px",
+            color: "#fff",
+            fontSize: "40px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Stop
+        </button>
+        <button
+          onClick={start}
+          style={{
+            border: "0px",
+            background: "#999",
+            margin: "60px",
+            padding: "20px",
+            color: "#fff",
+            fontSize: "40px",
+          }}
+        >
+          Start
+        </button>
+      </div>
     </div>
   );
 }
